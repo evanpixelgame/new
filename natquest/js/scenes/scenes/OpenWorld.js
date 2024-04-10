0a269221d1727105b1d6918e625c75af03c260fd
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-09 23:42:42 -0400

    Update OpenWorld.js

--- natquest/js/scenes/scenes/OpenWorld.js
+++ natquest/js/scenes/scenes/OpenWorld.js
@@ -1,6 +1,6 @@
 import { PlayerSprite } from '../PlayerSprite.js';
 import { sensorMapSet, createCollisionObjects } from '../collisionHandlers/mapSetter.js';
-import { sensorHandler } from '../collisionHandlers/collisionHandler.js';
+import { sensorHandler } from '../collisionHandlers/openWorldCollisionHandler.js';
 
 export default class OpenWorld extends Phaser.Scene {
   constructor() {
