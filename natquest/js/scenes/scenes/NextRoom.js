2931d505269501188b660e472eeda7e371316883
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-09 23:29:01 -0400

    Update NextRoom.js

--- natquest/js/scenes/scenes/NextRoom.js
+++ natquest/js/scenes/scenes/NextRoom.js
@@ -1,6 +1,6 @@
-import { PlayerSprite } from './PlayerSprite.js';
-import { sensorMapSet, createCollisionObjects } from './mapSetter.js';
-import { sensorHandler } from './collisionHandler.js';
+import { PlayerSprite } from '../PlayerSprite.js';
+import { sensorMapSet, createCollisionObjects } from '../collisionHandlers/mapSetter.js';
+import { sensorHandler } from '../collisionHandlers/collisionHandler.js';
 
 export default class NextRoom extends Phaser.Scene {
   constructor() {
