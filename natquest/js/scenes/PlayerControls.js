6453eddea241c1a2535853a2dbcb008e97b9263f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-10 00:25:35 -0400

    Update PlayerControls.js

--- natquest/js/scenes/PlayerControls.js
+++ natquest/js/scenes/PlayerControls.js
@@ -1,26 +1,26 @@
 export default class PlayerControls extends Phaser.Scene {
-    constructor() {
-        super({ key: 'PlayerControls' });
-         this.velocityChange = 2;// This is now an acceleration value for Matter.js
-    }
+  constructor() {
+    super({ key: 'PlayerControls' });
+    this.velocityChange = 2;// This is now an acceleration value for Matter.js
+  }
 
-    init(data) {
-        this.player = data.player;
-        this.playerBody = this.player.body;
-    }
+  init(data) {
+    this.player = data.player;
+    this.playerBody = this.player.body;
+  }
 
-    create() {
-        this.cursors = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            right: Phaser.Input.Keyboard.KeyCodes.D,
-        });
-    }
+  create() {
+    this.cursors = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+  }
 
   update(time, delta) {
     if (!this.player) return; // Guard clause
- //   console.log(this.player);
+    //   console.log(this.player);
     // Ensure we're accessing the Matter.js body directly
     const playerBody = this.player.body;
 
@@ -32,53 +32,53 @@ export default class PlayerControls extends Phaser.Scene {
     let velocityY = 0;
 
     if (this.cursors.left.isDown) {
-        velocityX = -velocity; // Move left
-   //     this.player.anims.play('walking-left', true);
+      velocityX = -velocity; // Move left
+      //     this.player.anims.play('walking-left', true);
     } else if (this.cursors.right.isDown) {
-        velocityX = velocity; // Move right
-   //       this.player.anims.play('walking-right', true);
-         
+      velocityX = velocity; // Move right
+      //       this.player.anims.play('walking-right', true);
+
     }
 
     if (this.cursors.up.isDown) {
-        velocityY = -velocity; // Move up
+      velocityY = -velocity; // Move up
       //   this.player.anims.play('walking-down', true);
     } else if (this.cursors.down.isDown) {
-        velocityY = velocity; // Move down
+      velocityY = velocity; // Move down
       //  this.player.anims.play('walking-up', true);
     }
 
     // Set the player's velocity directly
     // Ensure we're working with the Matter body, which might require adjusting how you access the player's body
-  //  Matter.Body.setVelocity(playerBody, { x: velocityX, y: velocityY });
-       if (this.player && this.player.body) {
+    //  Matter.Body.setVelocity(playerBody, { x: velocityX, y: velocityY });
+    if (this.player && this.player.body) {
       Matter.Body.setVelocity(this.player.body, { x: velocityX, y: velocityY });
     }
 
     // Optional: Reset to zero velocity if no key is pressed
-       if (this.player && this.player.body) {
-    if (!this.cursors.left.isDown && !this.cursors.right.isDown && !this.cursors.up.isDown && !this.cursors.down.isDown) {
+    if (this.player && this.player.body) {
+      if (!this.cursors.left.isDown && !this.cursors.right.isDown && !this.cursors.up.isDown && !this.cursors.down.isDown) {
         Matter.Body.setVelocity(playerBody, { x: 0, y: 0 });
-     //    this.player.anims.stop();
+        //    this.player.anims.stop();
+      }
     }
-       }
 
-          if (velocityX !== 0 || velocityY !== 0) {
-        if (velocityX > 0) {
-            this.player.anims.play('walking-right', true);
-        } else if (velocityX < 0) {
-            this.player.anims.play('walking-left', true);
-        } else if (velocityY < 0) {
-            this.player.anims.play('walking-down', true);
-        } else if (velocityY > 0) {
-            this.player.anims.play('walking-up', true);
-        }
+    if (velocityX !== 0 || velocityY !== 0) {
+      if (velocityX > 0) {
+        this.player.anims.play('walking-right', true);
+      } else if (velocityX < 0) {
+        this.player.anims.play('walking-left', true);
+      } else if (velocityY < 0) {
+        this.player.anims.play('walking-down', true);
+      } else if (velocityY > 0) {
+        this.player.anims.play('walking-up', true);
+      }
     } else {
-        // Stop animation when no movement
-        this.player.anims.stop();
+      // Stop animation when no movement
+      this.player.anims.stop();
     }
-         this.player.setRotation(0);
-}
+    this.player.setRotation(0);
+  }
 
 }
 
