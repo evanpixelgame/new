c0a233569381ad6fb063a343b6698aaa625c1e2e
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-10 00:30:09 -0400

    Update Settings.js

--- natquest/js/scenes/gameStartScenes/Settings.js
+++ natquest/js/scenes/gameStartScenes/Settings.js
@@ -3,31 +3,31 @@ class Settings extends Phaser.Scene {
     super({ key: 'Settings' });
   }
 
-  
+
   preload() {
-    
+
   }
 
   create() {
 
-        const vw = window.innerWidth;
-        const xMid = vw * .5;
-        const vh = window.innerHeight;
+    const vw = window.innerWidth;
+    const xMid = vw * .5;
+    const vh = window.innerHeight;
 
-     const graphics = this.add.graphics();
+    const graphics = this.add.graphics();
 
     // Fill the background with a solid color
     graphics.fillStyle(0xfdd5d5); // Specify the color (black in this case)
     graphics.fillRect(0, 0, this.game.config.width, this.game.config.height);
 
-      
-       const backdrop = this.add.graphics();
-        backdrop.fillStyle(0xE6E6FA, .7);
-        backdrop.lineStyle(4, 0x000000, 1);
-        backdrop.fillRect(vw * .05, vh * .05, vw * .9, vh * .85);
 
-     const backButton = this.add.text(xMid, vh * .8, 'Back', {
-      fontSize: '48px', 
+    const backdrop = this.add.graphics();
+    backdrop.fillStyle(0xE6E6FA, .7);
+    backdrop.lineStyle(4, 0x000000, 1);
+    backdrop.fillRect(vw * .05, vh * .05, vw * .9, vh * .85);
+
+    const backButton = this.add.text(xMid, vh * .8, 'Back', {
+      fontSize: '48px',
       fontFamily: 'knewave',
       fill: '#c92b23',
       padding: { x: 20, y: 20 },
@@ -35,7 +35,7 @@ class Settings extends Phaser.Scene {
       .setOrigin(0.5)
       .setInteractive();
 
-    
+
     // Set a callback function for the button click event
     backButton.on('pointerdown', function () {
       this.scene.start('StartMenu');
@@ -52,7 +52,7 @@ class Settings extends Phaser.Scene {
     })
       .setOrigin(0.5);
 
-    
+
   }
 }
 
