42644794dbe4c8c87037f4676c69fe00d8f1c2c4
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-10 02:07:40 -0400

    Update main.js

--- natquest/js/main.js
+++ natquest/js/main.js
@@ -17,7 +17,7 @@ import NextRoom from './scenes/scenes/NextRoom.js';
   import { Settings } from    "./scenes/gameStartScenes/Settings.js";
    import { NameSelect } from    "./scenes/gameStartScenes/NameSelect.js";
    import { CharSelect } from "./scenes/gameStartScenes/CharSelect.js";
-  import WelcomePlayer from  "./scenes/gameStartScenes/WelcomePlayer.js";
+  import { WelcomePlayer } from  "./scenes/gameStartScenes/WelcomePlayer.js";
   import OpenWorld from  "./scenes/scenes/OpenWorld.js";
    import { MobileControls } from "./scenes/MobileControls.js";
    import { GameUI } from    "./scenes/GameUI.js";
