940eca82ccd048a8dda2b4287c8a5d3cc35fd898
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-11 02:01:21 -0400

    Update openWorldCollisionHandler.js

--- natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
+++ natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
@@ -57,8 +57,9 @@ export function sensorHandler(scene, map, player, transitionSensors) {
 
             case 'BackToOpenWorld':
               console.log('take me back home daddy');
-        scene.scene.pause('NewScene'); // Stop or pause, depending on your needs
+     //   scene.scene.pause('NewScene'); // Stop or pause, depending on your needs
         scene.scene.resume('OpenWorld');
+        scene.scene.pause('NewScene');
               break;
 
 
