abeb70bdcb5e90112fe165449e0abebbfef8fc2f
evanpixelgame <155802426+evanpixelgame@users.noreply.github.com>
2024-04-11 17:00:37 -0400

    Update openWorldCollisionHandler.js

--- natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
+++ natquest/js/scenes/collisionHandlers/openWorldCollisionHandler.js
@@ -67,7 +67,7 @@ export function sensorHandler(scene, map, player, transitionSensors) {
        scene.scene.pause('NewScene');
       scene.scene.stop('PlayerControls');
        scene.scene.resume('OpenWorld', { sourceScene: 'NewScene' });
-       scene.scene.bringToTop('OpenWorld');
+       scene.scene.bringToTop('OpenWorld'); //instead of bringingopenworld to top, maybe setting visibility to 0? also maybe pause and resume would work with controls if player is passed continueously?
        scene.scene.launch('PlayerControls', { player: this.player });
     //   scene.scene.resume('PlayerCpmtrols');
       // scene.scene.bringToTop('PlayerControls');        
